dac5e0011640f6e0c5769a1e4911ba9992e6b9e9
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-10-07 11:22:39 +0200

    initial version

--- src/CurrencyAmounts.jl
+++ src/CurrencyAmounts.jl
@@ -1,5 +1,66 @@
 module CurrencyAmounts
 
-# Write your package code here.
+export Currency, CurrencyAmount, ExchangeRate
+
+import Base: +, -, *, /, ≈
+
+struct Currency{T} end
+Currency(c:: AbstractString) = Currency{Symbol(uppercase(c))}()
+
+Base.Broadcast.broadcastable(c:: Currency) = Ref(c) # treat it as a scalar in broadcasting
+
+struct CurrencyAmount{T <: Number, C <: Currency}
+    amount:: T
+end
+
+CurrencyAmount(x:: Number, ::C) where C <: Currency = CurrencyAmount{typeof(x), C}(x)
+
+Base.Broadcast.broadcastable(c:: CurrencyAmount) = Ref(c) # treat it as a scalar in broadcasting
+≈(x:: CurrencyAmount{T1, C}, y:: CurrencyAmount{T2, C}) where {C <: Currency, T1 <: Number, T2 <: Number} = x.amount ≈ y.amount
+
+# construction of CurrencyAmount using multiplication
+*(x:: Number, c:: Currency) = CurrencyAmount(x, c)
+
+## CurrencyAmount arithmetics
+
+# Addition and subtraction are only defined between equal currencies
++(x:: CurrencyAmount{T1, C}, y:: CurrencyAmount{T2, C}) where {C <: Currency, T1 <: Number, T2 <: Number} = CurrencyAmount(x.amount + y.amount, C())
+-(x:: CurrencyAmount{T1, C}, y:: CurrencyAmount{T2, C}) where {C <: Currency, T1 <: Number, T2 <: Number} = CurrencyAmount(x.amount - y.amount, C())
+
+*(x:: CurrencyAmount{T1, C}, y:: Number) where {C <: Currency, T1 <: Number} = CurrencyAmount(x.amount * y, C())
+*(x:: Number, y:: CurrencyAmount) = y*x
+
+# only division of a CurrencyAmount by a scalar is defined, not vice versa because this would give dimension of 1/Currency.
+/(x:: CurrencyAmount{T1, C}, y:: Number) where {C <: Currency, T1 <: Number} = CurrencyAmount(x.amount / y, C())
+
+struct ExchangeRate{T <: Number, BaseCurrency <: Currency, QuoteCurrency <: Currency}
+    rate:: T
+end
+
+Base.Broadcast.broadcastable(c:: ExchangeRate) = Ref(c) # treat it as a scalar in broadcasting
+
+## Exchange Rates
+ExchangeRate(x:: Number, ::BaseCurrency, ::QuoteCurrency) where {BaseCurrency <: Currency, QuoteCurrency <: Currency} = ExchangeRate{typeof(x), BaseCurrency, QuoteCurrency}(x)
+
+≈(x:: ExchangeRate{<: Number, C1, C2}, y:: ExchangeRate{<: Number, C1, C2}) where {C1 <: Currency, C2 <: Currency} = x.rate ≈ y.rate
+
+# in a currency fraction, the QuoteCurrency is the numerator and the BaseCurrency the denominator
+/(x:: CurrencyAmount{T1, C1}, y:: CurrencyAmount{T2, C2}) where {C1 <: Currency, C2 <: Currency, T1 <: Number, T2 <: Number} = ExchangeRate(x.amount/y.amount, C2(), C1())
+
+# exchange rate arithmetics
+*(x:: ExchangeRate{T1, C1, C2}, y:: Number) where {C1 <: Currency, C2 <: Currency, T1 <: Number} = ExchangeRate(x.rate * y, C1(), C2())
+*(x:: Number, y:: ExchangeRate) = y*x
+
+/(x:: ExchangeRate{T1, C1, C2}, y:: Number) where {C1 <: Currency, C2 <: Currency, T1 <: Number} = ExchangeRate(x.rate / y, C1(), C2())
+/(y:: Number, x:: ExchangeRate{T1, C1, C2}) where {C1 <: Currency, C2 <: Currency, T1 <: Number} = ExchangeRate(y / x.rate, C2(), C1())
++(x:: ExchangeRate{<: Number, C1, C2}, y:: ExchangeRate{<: Number, C1, C2}) where {C1 <: Currency, C2 <: Currency} = ExchangeRate(x.rate + y.rate, C1(), C2())
+-(x:: ExchangeRate{<: Number, C1, C2}, y:: ExchangeRate{<: Number, C1, C2}) where {C1 <: Currency, C2 <: Currency} = ExchangeRate(x.rate - y.rate, C1(), C2())
+
+# currency conversions
+
+*(x:: CurrencyAmount{<: Number, C1}, y:: ExchangeRate{<: Number, C1, C2}) where {C1 <: Currency, C2 <: Currency} = CurrencyAmount(x.amount * y.rate, C2())
+*(x:: ExchangeRate, y:: CurrencyAmount) = y*x
+
+/(x:: CurrencyAmount{<: Number, C2}, y:: ExchangeRate{<: Number, C1, C2}) where {C1 <: Currency, C2 <: Currency} = CurrencyAmount(x.amount / y.rate, C1())
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using CurrencyAmounts
 using Test
 
+const USD = Currency("usd")
+const EUR = Currency("eur")
+
 @testset "CurrencyAmounts.jl" begin
-    # Write your tests here.
+    
+    @test EUR == Currency{:EUR}()
+
+    @test 4USD == CurrencyAmounts.CurrencyAmount{Int64,Currency{:USD}}(4)
+    @test 4USD + 5.5USD == 9.5USD
+    @test 4USD - 5.5USD == -1.5USD
+    @test 4.5USD * 2 == 9.0USD
+    @test 4*4.5USD * 2 == 36.0USD
+    @test 8USD / 2 == 4.0USD
+    @test 8USD / 2 ≈ 4.0USD
+
+    # invalid operations
+    @test_throws MethodError 2 / 8USD
+    @test_throws MethodError 3USD * 4USD
+    @test_throws MethodError 4USD + 3EUR
+    @test_throws MethodError 4USD - 3EUR
+
+    # exchange rates
+    rate_eur_usd = ExchangeRate(1.2, EUR, USD)
+    @test rate_eur_usd == ExchangeRate{Float64,Currency{:EUR},Currency{:USD}}(1.2)
+    @test 2.4USD / 2EUR == rate_eur_usd
+    @test rate_eur_usd * 2 == 2.4USD / 1EUR
+    @test rate_eur_usd/2 == ExchangeRate{Float64,Currency{:EUR},Currency{:USD}}(0.6)
+    @test 1/rate_eur_usd == ExchangeRate{Float64,Currency{:USD},Currency{:EUR}}(1/1.2)
+    @test rate_eur_usd + 2*rate_eur_usd == 3*rate_eur_usd
+    @test ExchangeRate(2.2, EUR, USD) - rate_eur_usd ≈ ExchangeRate(1.0, EUR, USD)
+
+    # currency conversions
+    @test 3EUR * rate_eur_usd ≈ 3.6USD
+    @test rate_eur_usd * 3EUR == 3EUR * rate_eur_usd
+    @test_throws MethodError 3USD * rate_eur_usd # dimensions do not match
+    @test 3USD / rate_eur_usd == 2.5EUR
+
 end
